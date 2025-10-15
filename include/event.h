5fd58f0dd84d0e5e4f51e53d45a70467acba9f66
GinoLin980 <ppple1872@gmail.com>
2025-10-15 20:45:42 +0800

    made char fields in event.h be char* and added free_event

--- include/event.h
+++ include/event.h
@@ -1,12 +1,6 @@
 #ifndef EVENT_H
 #define EVENT_H
 
-#define MAX_ID_LEN 11
-#define MAX_DATE_LEN 11 // "xxxx-xx-xx\0"
-#define MAX_VEHICLE_LEN 100
-#define MAX_MISSION_LEN 100
-#define MAX_SITE_LEN 100
-
 typedef enum {
   SCHEDULED, //
   SUCCESS,   //
@@ -17,11 +11,11 @@ typedef enum {
 } Status;
 
 typedef struct {
-  char id[MAX_ID_LEN];
-  char date[MAX_DATE_LEN];
-  char vehicle[MAX_VEHICLE_LEN];
-  char mission[MAX_MISSION_LEN];
-  char site[MAX_SITE_LEN];
+  char *id;
+  char *date;
+  char *vehicle;
+  char *mission;
+  char *site;
   Status status;
 } Event;
 
@@ -30,5 +24,6 @@ Event *csv2event(char *csv_row, int *status);
 Event *new_event(const char *id, const char *date, const char *vehicle,
                  const char *site, const char *mission, Status status);
 Status str2status(const char *status_str);
+void free_event(Event event);
 
 #endif // !EVENT_H
