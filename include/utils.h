32b0b02facbe8f574a11b6ed8e4922b151c23508
lilyssMg <lilyssmg@gmail.com>
2025-10-17 04:44:32 -0600

    utils.h

--- include/utils.h
+++ include/utils.h
@@ -11,12 +11,4 @@ bool validate_date(const char *date_str);
 // Returns -1 if date1 < date2, 0 if equal, 1 if date1 > date2
 int compare_date(const char *date1, const char *date2);
 
-// Sets up signal handler for graceful shutdown
-void setup_signal_handler(void);
-
-// Cleans up resources (memory, files, etc.)
-void graceful_shutdown(int signal);
-
 #endif // UTILS_H
-
-
